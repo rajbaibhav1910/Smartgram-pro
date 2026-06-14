932c2eaed58adc495ef36747fed9f02a98f97511
rajbaibhav1910 <rajbaibhav1910@users.noreply.github.com>
2026-06-14 12:42:27 +0530

    docs: add professional README, remove hardcoded AWS credentials from config

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 import os
 
 class Config:
-    SECRET_KEY       = os.environ.get('SECRET_KEY', 'smartgram-rajbaibhav-secret-2024')
-    AWS_REGION       = 'us-east-1'
+    SECRET_KEY       = os.environ.get('SECRET_KEY', 'change-this-in-production')
+    AWS_REGION       = os.environ.get('AWS_REGION', 'us-east-1')
 
     # S3 Bucket for complaint images
-    S3_BUCKET        = 'smartgram-pro-images-rajbaibhav'
+    S3_BUCKET        = os.environ.get('S3_BUCKET', 'your-s3-bucket-name')
 
     # DynamoDB Table Names
     USERS_TABLE      = 'SmartGramUsers'
@@ -14,7 +14,7 @@ class Config:
     NOTICES_TABLE    = 'SmartGramNotices'
 
     # SNS Topic ARN for email alerts
-    SNS_TOPIC_ARN    = 'arn:aws:sns:us-east-1:070490636986:SmartGramAlerts'
+    SNS_TOPIC_ARN    = os.environ.get('SNS_TOPIC_ARN', 'arn:aws:sns:us-east-1:YOUR_ACCOUNT_ID:SmartGramAlerts')
 
     MAX_CONTENT_LENGTH = 1 * 1024 * 1024   # 1 MB max upload
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
